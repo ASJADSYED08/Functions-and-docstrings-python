9efe961d32bf111ca025193332360caed65da53f
ASJADSYED08 <hr49335@gmail.com>
2021-10-14 23:50:46 +0500

    first commit

---
+++ code.py
@@ -0,0 +1,21 @@
+a=5
+b=4
+c=sum((a,b))
+print(c)
+ 
+def function1(a,b):
+    print("sum of a and b is",a+b)
+function1(5,7)
+
+def function2(a,b):
+    average=(a+b)/2
+    print("avg",average)
+    return average
+function2(3,7)
+
+v=function2(5,7) 
+print("RETURN FILE")
+print(v)   
+
+
+    
